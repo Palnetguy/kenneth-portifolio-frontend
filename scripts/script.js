4eb4c879cdc7230bf31ae482edf969ee498479e0
IAN332 <sktechug@gmail.com>
2024-09-28 12:52:21 +0300

    Add endpoint

--- scripts/script.js
+++ scripts/script.js
@@ -286,7 +286,7 @@ const link_telegram = document.querySelector(".link_telegram");
 
 const userid = 2;
 const backend_url =
-  "https://backend.masaba-kenneth.info/api/user/2/";
+  "https://kenneth-portifolio-backend.up.railway.app/api/user/2/";
 
 function fetchProfile() {
   fetch(`${backend_url}profile/`)
